3cb7a4d6579bbfc15e2fe06fd7c65c5855b41870
Tom Shea <tom@shea.at>
2015-04-18 19:31:40 +0000

    basic platformer stuff... hit a roadblock with physics lib

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,7 +1,11 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
 var vss = require('vinyl-source-stream');
+var transform = require('vinyl-transform');
 var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var gm = require('gulp-gm');
+var rename = require('gulp-rename');
 
 function onError(err) {
   console.error(err.toString());
@@ -14,21 +18,36 @@ gulp.task('browserify', function() {
     .on('error', onError)
     .pipe(vss('index.js'))
     .pipe(buffer())
+    .pipe(uglify())
     .pipe(gulp.dest('./build'));
+});
+
+
+gulp.task('flip-images', function() {
+  gulp.src('./assets/*.png')
+    .pipe(gm(function(gmFile) {
+      return gmFile.flop();
+    }))
+    .pipe(rename({suffix: "_left"}))
+    .pipe(gulp.dest('./build/assets'));
 });
 
 gulp.task('deploy-js', ['browserify'], function() {
   return gulp.src('./build/*')
       .pipe(gulp.dest('/srv/http/tom.shea.at/ldjam32/js'));
 });
-gulp.task('deploy-static', function() {
+gulp.task('deploy-static', ['flip-images'], function() {
   gulp.src('./*.html')
     .pipe(gulp.dest('/srv/http/tom.shea.at/ldjam32'));
-  return gulp.src('./*.css')
+  gulp.src('./*.css')
     .pipe(gulp.dest('/srv/http/tom.shea.at/ldjam32'));
+  gulp.src('./assets/*')
+    .pipe(gulp.dest('/srv/http/tom.shea.at/ldjam32/assets'));
+  return gulp.src('./build/assets/*')
+    .pipe(gulp.dest('/srv/http/tom.shea.at/ldjam32/assets'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./index.js', ['deploy-js']);
+  gulp.watch('./*.js', ['deploy-js']);
   return gulp.watch(['./index.html', './style.css'], ['deploy-static']);
 });
--- index.js
+++ index.js
@@ -1,26 +1,60 @@
 var canvas;
 var context;
 
-var Physics = require('PhysicsJS');
+var Matter = require('matter-js');
+var Player = require('./player.js');
 
 var physOpts = {};
 
-var world = Physics(physOpts);
+var engine = Matter.Engine.create(null, physOpts);
+var world = engine.world;
+
+var InputManager = require('./input.js');
+
+var ents = [];
 
 window.addEventListener("load", function() {
   canvas = document.getElementById("display");
   context = canvas.getContext("2d");
+  context.imageSmoothingEnabled = false;
+  gameContext.context = context;
 
-  updateLoop();
+  setup();
+  updateLoop(performance.now());
 });
 
+function addEnt(ent) {
+  Matter.World.add(world, ent.makePhysicsObj());
+  ents.push(ent);
+}
 
-function updateLoop() {
-  world.step(Date.now());
-  render();
+var alivePlayer;
+function setup() {
+  var ground = Matter.Bodies.rectangle(canvas.width/2, canvas.height + 40, canvas.width, 80, {isStatic: true});
+  Matter.World.add(engine.world, ground);
+
+  alivePlayer = new Player(50, 50);
+  addEnt(alivePlayer);
+
+  InputManager.setup();
+}
+
+var lastFrameTime = performance.now();
+function updateLoop(now) {
+  var delta = now - lastFrameTime;
+  if(delta > 33)
+    delta = 33;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  Matter.Engine.update(engine, delta);
+  for(var i = 0; i < ents.length; i++) {
+    ents[i].update(context);
+  }
+  lastFrameTime = now;
   requestAnimationFrame(updateLoop);
 }
-function render() {
-  context.clearRect(0, 0, canvas.width, canvas.height);
-}
 
+window.gameContext = this;
+this.alivePlayer = alivePlayer;
+this.ents = ents;
+this.engine = engine;
+this.Matter = Matter;
