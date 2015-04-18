6c4f61325421d5673bd096997700d53d13563eca
Tom Shea <tom@shea.at>
2015-04-18 02:03:31 +0000

    Initial basic render/physics

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("hello world!");
+var canvas;
+var context;
+
+var Physics = require('PhysicsJS');
+
+var physOpts = {};
+
+var world = Physics(physOpts);
+
+window.addEventListener("load", function() {
+  canvas = document.getElementById("display");
+  context = canvas.getContext("2d");
+
+  updateLoop();
+});
+
+
+function updateLoop() {
+  world.step(Date.now());
+  render();
+  requestAnimationFrame(updateLoop);
+}
+function render() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
